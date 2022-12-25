263f629a1dfb709fdbecaa4829a10b8adb1c885f
theq03 <121336964+theq03@users.noreply.github.com>
2022-12-25 12:46:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ import os
 
 class Config(object):
     # Mandotory
-    APP_ID = int(os.environ.get("APP_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL"))
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
-    MONGODB_URL = os.environ.get("MONGODB_URL")
-    GOFILE_TOKEN = os.environ.get("GOFILE_TOKEN")
+    APP_ID = int(os.environ.get("2572163"))
+    API_HASH = os.environ.get("deede80ddff7842db6c90b5715635142")
+    BOT_TOKEN = os.environ.get("5896556129:AAFZHjNvmG1eq4VVO45ThhjUZdMKk0gh9mM")
+    LOGS_CHANNEL = int(os.environ.get("-1001623260624"))
+    BOT_OWNER = int(os.environ.get("1292898087"))
+    MONGODB_URL = os.environ.get("mongodb+srv://GDriveQbot:GDriveQbot@cluster0.2b9k6q5.mongodb.net/?retryWrites=true&w=majority")
     # Optional
+    GOFILE_TOKEN = os.environ.get("GOFILE_TOKEN")
     MAX_DOWNLOAD_SIZE = int(os.environ.get("MAX_DOWNLOAD_SIZE")) if os.environ.get("MAX_DOWNLOAD_SIZE") else 10737418240
     # Constents
     DOWNLOAD_LOCATION = f"{os.path.dirname(__file__)}/NexaBots"
